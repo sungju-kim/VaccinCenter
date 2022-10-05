04f995c962f6ed53aba7fe3243c2d86cec66d612
sungju-kim <wnwn0719@gmail.com>
2022-10-05 22:59:05 +0900

    Feat: NetworkContainer 구현
    #18

---
+++ VaccinCenter/VaccinCenter/Source/Model/Common/NetworkContainer.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkContainer.swift
+//  VaccinCenter
+//
+//  Created by dale on 2022/10/05.
+//
+
+import Foundation
+
+final class NetworkContainer {
+    private init() {}
+
+    static var shared = NetworkContainer()
+
+    lazy var centersRepository: CentersRepository = CentersRepositoryImpl()
+}
