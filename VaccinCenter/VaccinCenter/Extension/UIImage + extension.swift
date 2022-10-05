38fabc816ad278fccc416ee5d9c3c6ef86a245d3
sungju-kim <wnwn0719@gmail.com>
2022-10-05 19:10:24 +0900

    Add: UIImage + extension 추가

---
+++ VaccinCenter/VaccinCenter/Extension/UIImage + extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIImage + extension.swift
+//  VaccinCenter
+//
+//  Created by dale on 2022/10/05.
+//
+
+import UIKit.UIImage
+
+extension UIImage {
+    enum Custom {
+        static let building = UIImage(named: "building")
+        static let chat = UIImage(named: "chat")
+        static let hospital = UIImage(named: "hospital")
+        static let placeholder = UIImage(named: "placeholder")
+        static let telephone = UIImage(named: "telephone")
+        static let topAlignment = UIImage(named: "top-alignment")
+    }
+}
