aa329524a1ea049266b7f0256bce9adc3e1ea5ee
sungju-kim <wnwn0719@gmail.com>
2022-10-05 22:04:06 +0900

    Feat: Requestable 프로토콜 구현
    #15

--- VaccinCenter/VaccinCenter/Source/Model/API/Requetable.swift
+++ VaccinCenter/VaccinCenter/Source/Model/API/Requetable.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import Alamofire
+
+protocol Requetable {
+    var baseURL: URL? { get }
+    var path: String { get }
+    var url: URL? { get }
+    var headers: HTTPHeaders { get }
+    var method: Alamofire.HTTPMethod { get }
+}
