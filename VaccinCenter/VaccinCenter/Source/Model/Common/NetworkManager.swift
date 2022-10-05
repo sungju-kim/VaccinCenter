83cad4394ae54fdd3f0c11e862c9ab68c88cee9e
sungju-kim <wnwn0719@gmail.com>
2022-10-05 22:37:32 +0900

    Add: NetworkManager 구현
    #16

---
+++ VaccinCenter/VaccinCenter/Source/Model/Common/NetworkManager.swift
@@ -0,0 +1,39 @@
+//
+//  NetworkManager.swift
+//  VaccinCenter
+//
+//  Created by dale on 2022/10/05.
+//
+
+import Foundation
+import RxSwift
+import Alamofire
+
+final class NetworkManager {
+    func request(endPoint: Requetable) -> Single<Data> {
+        return Single.create { observer in
+            guard let url = endPoint.url else {
+                observer(.failure(NetworkError.invalidURL))
+                return Disposables.create()
+            }
+
+            let request: DataRequest = AF.request(url,
+                                                  method: endPoint.method,
+                                                  parameters: endPoint.parameter,
+                                                  headers: endPoint.headers)
+
+            request
+                .validate(statusCode: 200..<300)
+                .response { response in
+                    if let data = response.data {
+                        observer(.success(data))
+                    } else {
+                        observer(.failure(NetworkError.emptyData))
+                    }
+                }
+
+            observer(.failure(NetworkError.badRequest))
+            return Disposables.create()
+        }
+    }
+}
