50b29ed3bd2ff0e5e45511a07881e551d0d4f826
sungju-kim <wnwn0719@gmail.com>
2022-10-05 21:59:00 +0900

    Add: Center Domain 추가

---
+++ VaccinCenter/VaccinCenter/Source/Model/Domain/Center.swift
@@ -0,0 +1,19 @@
+//
+//  Center.swift
+//  VaccinCenter
+//
+//  Created by dale on 2022/10/05.
+//
+
+import Foundation
+
+struct Center {
+    let id: String
+    let centerName: String
+    let facilityName: String
+    let address: String
+    let latitude: Double
+    let longitued: Double
+    let updatedAt: Date
+    let phoneNumber: String
+}
