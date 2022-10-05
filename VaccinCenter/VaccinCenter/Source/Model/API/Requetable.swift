171c02f71233bd718f105e7ec3863930916e1882
sungju-kim <wnwn0719@gmail.com>
2022-10-05 22:11:56 +0900

    Feat: EndPoint 파라미터 추가 - pageNumber - apiKey
    Closes #15

--- VaccinCenter/VaccinCenter/Source/Model/API/Requetable.swift
+++ VaccinCenter/VaccinCenter/Source/Model/API/Requetable.swift
@@ -9,9 +9,11 @@ import Foundation
 import Alamofire
 
 protocol Requetable {
+    var apiKey: String { get }
     var baseURL: URL? { get }
     var path: String { get }
     var url: URL? { get }
     var headers: HTTPHeaders { get }
+    var parameter: [String: Any] { get }
     var method: Alamofire.HTTPMethod { get }
 }
--- VaccinCenter/VaccinCenter/Source/Model/API/VaccinCenterEndPoint.swift
+++ VaccinCenter/VaccinCenter/Source/Model/API/VaccinCenterEndPoint.swift
@@ -9,10 +9,17 @@ import Foundation
 import Alamofire
 
 enum VaccinCenterEndPoint {
-    case centers
+    case centers(pageNumber: Int)
 }
 
 extension VaccinCenterEndPoint: Requetable {
+    var apiKey: String {
+        guard let file = Bundle.main.path(forResource: "Info", ofType: "plist") else { return "" }
+        guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
+        guard let key = resource["API_KEY"] as? String else { fatalError("Info.plist에 API Key를 설정해주세요.")}
+        return key
+    }
+
     var baseURL: URL? {
         return URL(string: "api.odcloud.kr/api")
     }
@@ -30,6 +37,12 @@ extension VaccinCenterEndPoint: Requetable {
 
     var headers: Alamofire.HTTPHeaders {
         return [HTTPHeader.accept("application/json")]
+    }
+    var parameter: [String: Any] {
+        switch self {
+        case .centers(let pageNumber):
+            return ["page": pageNumber, "serviceKey": apiKey]
+        }
     }
 
     var method: Alamofire.HTTPMethod {
