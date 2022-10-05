159476a70785e95192dc3ea7a4c015577eafa226
sungju-kim <wnwn0719@gmail.com>
2022-10-05 19:10:52 +0900

    Add: Constraint 추가

---
+++ VaccinCenter/VaccinCenter/Support/Constraint.swift
@@ -0,0 +1,16 @@
+//
+//  Constraint.swift
+//  VaccinCenter
+//
+//  Created by dale on 2022/10/05.
+//
+
+import CoreGraphics
+
+enum Constraint {
+    static let min: CGFloat = 8
+    static let semiRegular: CGFloat = 12
+    static let regular: CGFloat = 16
+    static let semiMax: CGFloat = 32
+    static let max: CGFloat = 48
+}
