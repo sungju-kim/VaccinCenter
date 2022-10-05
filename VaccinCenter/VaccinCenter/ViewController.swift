f65d67ab364397196c6341141568281efa9b12b9
sungju-kim <wnwn0719@gmail.com>
2022-10-05 19:11:14 +0900

    Add: ScrollTopButton 추가

--- VaccinCenter/VaccinCenter/ViewController.swift
+++ VaccinCenter/VaccinCenter/ViewController.swift
@@ -15,11 +15,39 @@ final class CenterListViewController: UIViewController {
         return tableView
     }()
 
+    private let scrollTopButton: UIButton = {
+        let button: UIButton
+        if #available(iOS 15.0, *) {
+            var configuration = UIButton.Configuration.plain()
+            configuration.image = .Custom.topAlignment
+            configuration.contentInsets = .init(top: Constraint.semiRegular,
+                                                leading: Constraint.semiRegular,
+                                                bottom: Constraint.semiRegular,
+                                                trailing: Constraint.semiRegular)
+
+            button = UIButton(configuration: configuration)
+        } else {
+            button = UIButton()
+            button.setImage(.Custom.topAlignment, for: .normal)
+            button.contentEdgeInsets = UIEdgeInsets(top: Constraint.semiRegular,
+                                                    left: Constraint.semiRegular,
+                                                    bottom: Constraint.semiRegular,
+                                                    right: Constraint.semiRegular)
+        }
+        button.backgroundColor = .systemBackground
+        button.layer.shadowColor = UIColor.gray.cgColor
+        button.layer.shadowOpacity = 1.0
+        button.layer.shadowOffset = CGSize.zero
+        button.layer.cornerRadius = button.intrinsicContentSize.height / 2
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setAttribute()
         layoutTableView()
+        layoutScrollTopButton()
     }
 }
 
@@ -40,6 +68,15 @@ private extension CenterListViewController {
 
         tableView.snp.makeConstraints { make in
             make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+
+    func layoutScrollTopButton() {
+        view.addSubview(scrollTopButton)
+
+        scrollTopButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(Constraint.regular)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(Constraint.regular)
         }
     }
 }
