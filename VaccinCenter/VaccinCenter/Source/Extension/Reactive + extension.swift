ca8d9a9813165b67f6d1e1c0dc64cad4d4e48b02
sungju-kim <wnwn0719@gmail.com>
2022-10-06 02:13:53 +0900

    Add: Reactive + extension 추가 및 reachedBottom 구현

---
+++ VaccinCenter/VaccinCenter/Source/Extension/Reactive + extension.swift
@@ -0,0 +1,25 @@
+//
+//  Reactive + extension.swift
+//  VaccinCenter
+//
+//  Created by dale on 2022/10/06.
+//
+
+import UIKit.UIScrollView
+import RxSwift
+import RxCocoa
+
+extension Reactive where Base: UIScrollView {
+    func reachedBottom() -> ControlEvent<Void> {
+        let source = contentOffset.map { contentOffset in
+            if base.contentSize.height == 0 { return false }
+            let visibleHeight = base.frame.height - base.contentInset.top - base.contentInset.bottom
+            let yPosition = contentOffset.y + base.contentInset.top
+            let threshold = base.contentSize.height - visibleHeight
+            return yPosition >= threshold }
+            .distinctUntilChanged()
+            .filter { $0 }
+            .map { _ in }
+        return ControlEvent(events: source)
+    }
+}
