660ba40ec386c362aa002bf69faba2f2fd52cb3c
sungju-kim <wnwn0719@gmail.com>
2022-10-05 21:58:43 +0900

    Add: CenterResponseDTO, CenterDTO 추가

---
+++ VaccinCenter/VaccinCenter/Source/Model/DTO/CenterResponseDTO.swift
@@ -0,0 +1,29 @@
+//
+//  CenterResponseDTO.swift
+//  VaccinCenter
+//
+//  Created by dale on 2022/10/05.
+//
+
+import Foundation
+
+struct CenterResponseDTO: Decodable {
+    let data: [CenterDTO]
+}
+
+struct CenterDTO: Decodable {
+    let id: String
+    let centerName: String
+    let facilityName: String
+    let address: String
+    let latitude: Double
+    let longitued: Double
+    let updatedAt: Date
+    let phoneNumber: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, centerName, facilityName, address, updatedAt, phoneNumber
+        case latitude = "lat"
+        case longitued = "lng"
+    }
+}
