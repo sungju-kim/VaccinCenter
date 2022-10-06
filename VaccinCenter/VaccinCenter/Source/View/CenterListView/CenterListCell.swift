ee69305e3161ba80dcd5d09e303d62e8e8dde813
sungju-kim <wnwn0719@gmail.com>
2022-10-06 21:33:57 +0900

    Refactor: Label font size 수정

--- VaccinCenter/VaccinCenter/Source/View/CenterListView/CenterListCell.swift
+++ VaccinCenter/VaccinCenter/Source/View/CenterListView/CenterListCell.swift
@@ -18,6 +18,7 @@ final class CenterListCell: UITableViewCell {
             let label = UILabel()
             label.text = $0
             label.textColor = .gray
+            label.font = .systemFont(ofSize: 12, weight: .regular)
             label.setContentHuggingPriority(.init(251), for: .horizontal)
             label.setContentCompressionResistancePriority(.init(1000), for: .horizontal)
             stackView.addArrangedSubview(label)
@@ -32,6 +33,7 @@ final class CenterListCell: UITableViewCell {
         let stackView = UIStackView()
         [titleLabel, buildingLabel, addressLabel, updatedLabel].forEach {
             $0.textColor = .black
+            $0.font = .systemFont(ofSize: 12, weight: .regular)
             stackView.addArrangedSubview($0)
         }
         stackView.axis = .vertical
