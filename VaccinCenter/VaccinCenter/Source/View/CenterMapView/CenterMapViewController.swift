dafb2b455e10007ce6f7e9481431362eb31f2203
sungju-kim <wnwn0719@gmail.com>
2022-10-06 20:18:04 +0900

    Feat: CenterMapView 이동시 해당 Center mapView에 표시하는 로직 구현

--- VaccinCenter/VaccinCenter/Source/View/CenterMapView/CenterMapViewController.swift
+++ VaccinCenter/VaccinCenter/Source/View/CenterMapView/CenterMapViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import MapKit
+import RxSwift
+import RxAppState
 
 final class CenterMapViewController: UIViewController {
+    private let disposeBag = DisposeBag()
+
     private var viewModel: CenterMapViewModel?
+
+    private let mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.showsUserLocation = true
+        return mapView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        title = "지도"
+
+        layoutMapView()
+    }
+
+    private func createAnnotation(_ marker: Marker) {
+        mapView.addAnnotation(marker)
     }
 }
 
@@ -22,5 +40,29 @@ final class CenterMapViewController: UIViewController {
 extension CenterMapViewController {
     func configure(with viewModel: CenterMapViewModel) {
         self.viewModel = viewModel
+
+        viewModel.didLoadMarker
+            .bind(onNext: createAnnotation)
+            .disposed(by: disposeBag)
+
+        viewModel.didSetRegion
+            .bind(onNext: mapView.setRegion)
+            .disposed(by: disposeBag)
+
+        rx.viewDidLoad
+            .bind(to: viewModel.viewDidLoad)
+            .disposed(by: disposeBag)
+    }
+}
+
+// MARK: - Layout Section
+
+private extension CenterMapViewController {
+    func layoutMapView() {
+        view.addSubview(mapView)
+
+        mapView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
     }
 }
--- VaccinCenter/VaccinCenter/Source/View/CenterMapView/CenterMapViewModel.swift
+++ VaccinCenter/VaccinCenter/Source/View/CenterMapView/CenterMapViewModel.swift
@@ -6,10 +6,26 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
+import MapKit.MKGeometry
 
 final class CenterMapViewModel {
+    private let disposeBag = DisposeBag()
+
+    let viewDidLoad = PublishRelay<Void>()
+    let didLoadMarker = PublishRelay<Marker>()
+    let didSetRegion = PublishRelay<(MKCoordinateRegion, Bool)>()
 
     init(center: Center) {
+        viewDidLoad
+            .map { Marker(center: center) }
+            .bind(to: didLoadMarker)
+            .disposed(by: disposeBag)
 
+        didLoadMarker
+            .map { (MKCoordinateRegion(center: $0.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)), true) }
+            .bind(to: didSetRegion)
+            .disposed(by: disposeBag)
     }
 }
