42082fbb284c66550c643943afb3e8b1da9f5925
sungju-kim <wnwn0719@gmail.com>
2022-10-07 16:26:42 +0900

    Add: RxCoreLocation 추가 및 관련로직 변경

--- VaccinCenter/VaccinCenter/Source/Model/Repository/LocationRepositoryImpl.swift
+++ VaccinCenter/VaccinCenter/Source/Model/Repository/LocationRepositoryImpl.swift
@@ -8,34 +8,33 @@
 import Foundation
 import MapKit.MKAnnotation
 import RxRelay
+import RxSwift
+import RxCoreLocation
 
 final class LocationRepositoryImpl: NSObject, CLLocationManagerDelegate, LocationRepository {
+    private let disposeBag = DisposeBag()
     private(set) var didLoadLocation = PublishRelay<CLLocationCoordinate2D>()
 
-    private let locationManager = CLLocationManager()
+    private lazy var locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        return manager
+    }()
 
     override init() {
         super.init()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
+
+        Observable.merge(
+            locationManager.rx.didUpdateLocations.compactMap { $0.locations.last?.coordinate},
+            locationManager.rx.didChangeAuthorization.compactMap { $0.manager.location?.coordinate }
+        )
+        .bind(to: didLoadLocation)
+        .disposed(by: disposeBag)
     }
 
     func updateLocation() {
         locationManager.startUpdatingLocation()
-    }
-
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-    }
-
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        locationManager.stopUpdatingLocation()
-        guard let location = locations.last else { return }
-        didLoadLocation.accept(location.coordinate)
-    }
-
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        guard let location = manager.location else { return }
-        didLoadLocation.accept(location.coordinate)
     }
 }
