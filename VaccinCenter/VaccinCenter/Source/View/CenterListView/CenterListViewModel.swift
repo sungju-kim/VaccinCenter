0b02c7938fc7d32072bb003fb5954a28cecd8408
sungju-kim <wnwn0719@gmail.com>
2022-10-07 12:52:01 +0900

    Refactor: 가독성 개선

--- VaccinCenter/VaccinCenter/Source/View/CenterListView/CenterListViewModel.swift
+++ VaccinCenter/VaccinCenter/Source/View/CenterListView/CenterListViewModel.swift
@@ -23,19 +23,9 @@ final class CenterListViewModel {
     let prepareForPush = PublishRelay<CenterDetailViewModel>()
 
     init() {
-        bind()
-    }
-}
-
-// MARK: - Bind
-
-private extension CenterListViewModel {
-    func bind() {
         Observable.merge(viewDidLoad.asObservable(), didScrollBottom.asObservable())
             .withLatestFrom(didLoadCenter) { $1.count / 10 + 1 }
-            .withUnretained(self)
-            .flatMapLatest { viewModel, page in
-                viewModel.repository.requestCenters(page: page)}
+            .flatMapLatest(repository.requestCenters)
             .scan([Center](), accumulator: +)
             .map { $0.sorted { $0.updatedAt > $1.updatedAt } }
             .bind(to: didLoadCenter)
