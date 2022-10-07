7f3596c9ed9d409e563edec777e375c9dd6295bc
sungju-kim <wnwn0719@gmail.com>
2022-10-07 13:56:16 +0900

    Refactor: 불필요 변수 삭제

--- VaccinCenter/VaccinCenter/Source/Extension/UIImage + extension.swift
+++ VaccinCenter/VaccinCenter/Source/Extension/UIImage + extension.swift
@@ -9,11 +9,6 @@ import UIKit.UIImage
 
 extension UIImage {
     enum Custom {
-        static let building = UIImage(named: "building")
-        static let chat = UIImage(named: "chat")
-        static let hospital = UIImage(named: "hospital")
-        static let placeholder = UIImage(named: "placeholder")
-        static let telephone = UIImage(named: "telephone")
         static let topAlignment = UIImage(named: "top-alignment")
     }
 }
